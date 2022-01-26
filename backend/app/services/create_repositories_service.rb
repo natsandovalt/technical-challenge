3fbf6b291554f99c723489fbaca1d1ac6031b24d
Natalia Sandoval <natsandovalt@gmail.com>
2022-01-26 14:36:59 -0600

    feat: :sparkles: repositories service created

---
+++ backend/app/services/create_repositories_service.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class CreateRepositoriesService
+  include FaradayHelper
+
+  def initialize(username:)
+    @username = username
+  end
+
+  def call
+    user = User.find_by(username: @username)
+    repos = connection.get("#{ENV['GITHUB_URL']}/users/#{@username}/repos", { per_page: 100 }).body
+
+    raise(GithubUserNotFoundError, "Could't find github user: #{@username}!") if user_not_found(repos)
+
+    create_repositories(repos, user) if user.repositories.empty?
+
+    user.repositories
+  end
+
+  private
+
+  def create_repositories(repos, user)
+    Repository.transaction do
+      repos.each do |repo|
+        Repository.create(
+          github_id: repo['id'],
+          name: repo['name'],
+          full_name: repo['full_name'],
+          private: repo['private'],
+          html_url: repo['html_url'],
+          description: repo['description'],
+          fork: repo['fork'],
+          url: repo['url'],
+          user: user
+        )
+      end
+    end
+  end
+
+  def user_not_found(repos)
+    !repos.is_a?(Array) && repos['message'] == 'Not Found'
+  end
+end
