1b5cc221a78f87ed6fd5c9d60c56cb2a065c8e94
Natalia Sandoval <natsandovalt@gmail.com>
2022-01-26 14:38:31 -0600

    refactor: :recycle: repositories controller and spec refactored

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -4,28 +4,21 @@ module Api
   module V1
     class RepositoriesController < ApplicationController
       def index
-        conn = Faraday.new do |f|
-          f.request :authorization, 'Bearer', ENV['GITHUB_TOKEN']
-          f.request :json # encode req bodies as JSON
-          f.request :retry # retry transient failures
-          f.response :follow_redirects # follow redirects
-          f.response :json # decode response bodies as JSON
-        end
-        user = conn.get("https://api.github.com/users/#{user_params}").body
-        repos = conn.get("https://api.github.com/users/#{user_params}/repos",
-                         { per_page: 100, sort: 'updated' }).body
-        db_user = User.all.find { |u| u.github_id == user['id'] }
-        if db_user.nil?
-          db_user = User.create({ github_id: user['id'], login: user['login'], url: user['html_url'], name: user['name'],
-                                  email: user['email'], avatar_url: user['avatar_url'], repositories: repos })
-        end
-        render json: db_user.repositories
+        user = CreateUserService.new(username: user_params).call
+        CreateRepositoriesService.new(username: user_params).call
+        render json: user.repositories
+      rescue GithubUserNotFoundError => e
+        render json: payload(e.message, 400), status: :bad_request
       end
 
       private
 
       def user_params
         params.require(:user_id)
+      end
+
+      def payload(message, status)
+        { message: message, status: status }
       end
     end
   end
--- backend/spec/requests/api/v1/repositories_spec.rb
+++ backend/spec/requests/api/v1/repositories_spec.rb
@@ -4,11 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Repositories', type: :request do
   describe 'GET /index' do
-    it 'gets repositories for yknx4' do
-      get api_v1_user_repositories_path(user_id: 'yknx4')
+    it 'gets repositories for natsandovalt' do
+      get api_v1_user_repositories_path(user_id: 'natsandovalt')
       body = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(body.size).to eq(100)
+      expect(body.size).to eq(10)
+    end
+
+    it 'get bad request status when user does not exist in github' do
+      get api_v1_user_repositories_path(user_id: 'nonexistentuser')
+      expect(response).to have_http_status(400)
     end
   end
 end
