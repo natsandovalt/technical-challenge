8960dc0b1856f0d1833fe3afd3bb13182593a289
Natalia Sandoval <natsandovalt@gmail.com>
2022-01-25 18:23:29 -0600

    test: :white_check_mark: user factory updated and tests added to user spec

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,5 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = build_user
+
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without username' do
+    user = build_user
+    user.username = nil
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without github_id' do
+    user = build_user
+    user.github_id = nil
+
+    expect(user).to_not be_valid
+  end
+
+  def build_user
+    build(:user)
+  end
 end
