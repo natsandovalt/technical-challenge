585e09f62aa789dee07cecc8f51ac0d0924bd318
Natalia Sandoval <natsandovalt@gmail.com>
2022-01-25 22:07:43 -0600

    fix: :bug: fixed urls for requests

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -5,14 +5,14 @@ module Api
     class RepositoriesController < ApplicationController
       def index
         conn = Faraday.new do |f|
-          f.request :authorization, 'Bearer', Figaro.env.GITHUB_TOKEN
+          f.request :authorization, 'Bearer', ENV['GITHUB_TOKEN']
           f.request :json # encode req bodies as JSON
           f.request :retry # retry transient failures
           f.response :follow_redirects # follow redirects
           f.response :json # decode response bodies as JSON
         end
-        user = conn.get("https://api.github.com/user?user=#{user_params}").body
-        repos = conn.get("https://api.github.com/user/repos?user=#{user_params}",
+        user = conn.get("https://api.github.com/users/#{user_params}").body
+        repos = conn.get("https://api.github.com/users/#{user_params}/repos",
                          { per_page: 100, sort: 'updated' }).body
         db_user = User.all.find { |u| u.github_id == user['id'] }
         if db_user.nil?
