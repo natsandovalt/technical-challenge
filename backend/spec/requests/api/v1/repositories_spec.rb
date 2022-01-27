89624eeca9095599b4b5e41eccfa7cde34205fa9
Natalia Sandoval <natsandovalt@gmail.com>
2022-01-26 20:34:59 -0600

    test: :white_check_mark: tests added for filtering feature

--- backend/spec/requests/api/v1/repositories_spec.rb
+++ backend/spec/requests/api/v1/repositories_spec.rb
@@ -15,5 +15,19 @@ RSpec.describe 'Api::V1::Repositories', type: :request do
       get api_v1_user_repositories_path(user_id: 'nonexistentuser')
       expect(response).to have_http_status(400)
     end
+
+    it 'get repositories of natsandovalt that matches with technical-challenge' do
+      get api_v1_user_repositories_path(user_id: 'natsandovalt', query: 'technical-challenge')
+      body = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(body.size).to eq(1)
+    end
+
+    it 'get repositories of natsandovalt that matches with ejercicio' do
+      get api_v1_user_repositories_path(user_id: 'natsandovalt', query: 'ejercicio')
+      body = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(body.size).to eq(2)
+    end
   end
 end
