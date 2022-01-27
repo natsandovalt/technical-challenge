522189908228fe7d5eca66fa8f9cfc9b91680e79
Natalia Sandoval <natsandovalt@gmail.com>
2022-01-26 20:34:12 -0600

    feat: :sparkles: searchkick added to repository model

--- backend/app/models/repository.rb
+++ backend/app/models/repository.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Repository < ApplicationRecord
+  searchkick word_middle: %i[name full_name description]
+
   belongs_to :user
 
   validates :github_id, presence: true
