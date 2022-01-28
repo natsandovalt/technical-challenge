e9819c462f0d7a9ca3508ee702343f91c8f75a12
Natalia Sandoval <natsandovalt@gmail.com>
2022-01-27 18:40:16 -0600

    feat: :sparkles: logs added to controllers

--- backend/app/controllers/api/v1/repositories_controller.rb
+++ backend/app/controllers/api/v1/repositories_controller.rb
@@ -3,11 +3,16 @@
 module Api
   module V1
     class RepositoriesController < ApplicationController
+      include LoggerHelper
+
       def index
+        log_info('Creating user')
         user = CreateUserService.new(username: user_params).call
+        log_info('Creating repositories')
         CreateRepositoriesService.new(username: user_params).call
         render json: repositories(user)
       rescue GithubUserNotFoundError => e
+        log_error(e.message)
         render json: payload(e.message, 400), status: :bad_request
       end
 
--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,16 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      include LoggerHelper
+
       def index
+        log_info('Creating user')
         user = CreateUserService.new(username: user_params).call
+        log_info('Creating repositories')
         CreateRepositoriesService.new(username: user_params).call
         render json: user.as_json.except('repositories')
       rescue GithubUserNotFoundError => e
+        log_error(e.message)
         render json: payload(e.message, 400), status: :bad_request
       end
 
